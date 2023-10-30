8704a36349a0f0777908590fed6c32bbd8b75d7d
Marvin0555 <60906316+Marvin0555@users.noreply.github.com>
2023-10-29 23:50:16 -0400

    Update test2_opencv.py
    alteração na região de busca da listra

--- código_opencv/test2_opencv.py
+++ código_opencv/test2_opencv.py
@@ -2,19 +2,18 @@ import cv2
 import numpy as np
 import time
 
-cap = cv2.VideoCapture(2)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+cap = cv2.VideoCapture(3)
 Starus_esteria = "OFF"
 
 while True:
 
     _, frame = cap.read()
     height, width, dd = frame.shape
-    belt = frame[0: 720, 500: 780 ]
-    mask = cv2.cvtColor(belt, cv2.COLOR_BGR2HSV)
     cx = int(width // 2)
     cy = int(height // 2)
+
+    belt = frame[0: height, cx-100: cx+100 ]
+    mask = cv2.cvtColor(belt, cv2.COLOR_BGR2HSV)
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -28,7 +27,7 @@ while True:
     # Red color
     low_red = np.array([99, 108, 131]) #np.array([161, 155, 84])
     high_red = np.array([180, 255, 255]) #np.array([179, 255, 255])
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    red_mask = cv2.inRange(mask, low_red, high_red)
     
     #erosão e dilatação
     erodeElement = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
@@ -43,54 +42,50 @@ while True:
     
 
     # Extraia a região de interesse
-    roi = red_mask[cy-1:cy+1, cx-99:cx+100]
+    roi = red_mask[cy-1:cy+1, 1:200]
 
     # Calcule o centro dos pixels na ROI
     posicao_centro = np.argwhere(roi == 255)
 
     red_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print(red_contours)
+    if red_contours:
+        # Se houver contornos encontrados
+        for cnt in red_contours:
+            # Faça algo com os contornos
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            
+            area = cv2.contourArea(cnt)
+            if area > 400:
+                cv2.rectangle(frame, (220 + x, y), (220 +x + w, y + h), (0, 0, 255), 2)
+                #cv2.putText(belt, str(area), (x, y), 1, 1, (0,255,0))
+                moment = cv2.moments(cnt)
+                area = moment['m00']
+                qcentro_x = int(moment['m10']/area)
+                qcentro_y = int(moment['m01']/area)
+                cv2.circle(frame, (220 + qcentro_x , qcentro_y), 10, (0,255,0), -1)
 
-    for cnt in red_contours:
-        (x, y, w, h) = cv2.boundingRect(cnt)
-        
-        area = cv2.contourArea(cnt)
-        if area > 400:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            #cv2.putText(belt, str(area), (x, y), 1, 1, (0,255,0))
-            moment = cv2.moments(cnt)
-            area = moment['m00']
-            qcentro_x = int(moment['m10']/area)
-            qcentro_y = int(moment['m01']/area)
-            cv2.circle(frame, (qcentro_x , qcentro_y), 10, (0,255,0), -1)
 
-            # Coordenadas absolutas dos pontos extremos esquerdo e direito do retângulo em relação ao frame
-            ponto_esquerdo_x = x 
-            ponto_direito_x = x + w
-
-            # Coordenadas absolutas da lina
-            centro_x = 140
-
-            # Calcula a distância entre os pontos extremos e o centro da linha
-            distancia_esquerdo = abs(centro_x - ponto_esquerdo_x)
-            distancia_direito = abs(centro_x - ponto_direito_x)
-
-            #cv2.putText(frame, f"Distância esquerda: {distancia_esquerdo}, Distância direita: {distancia_direito}", (0, 50), 2, 1, (0, 0, 255), 2)
-
-    # Verifique se há pixels brancos na ROI
-    if len(posicao_centro) > 0:
-        y,x = roi.shape
-        cx_roi = int(x//2)
-        cy_roi = int(y//2)
-        centro_x = np.mean(posicao_centro[:, 1])
-        centro_y = np.mean(posicao_centro[:, 0])
-
-        # Calcule a relação em relação ao centro da imagem
-        relacao = (int(centro_x) - cx_roi ) / cx_roi
-        cv2.putText(frame, f"Distancia x: {int(centro_x)}, Distancia y: {int(centro_y)}", (0, 50), 2, 1, (0, 0, 255), 2)
-        print("Relação em relação ao centro da imagem (-1 a 1):", relacao)
+        # Verifique se há pixels brancos na ROI
+        if len(posicao_centro) > 0:
+            y,x = roi.shape
+            cx_roi = int(x//2)
+            cy_roi = int(y//2)
+            centro_x = np.mean(posicao_centro[:, 1])
+            centro_y = np.mean(posicao_centro[:, 0])
+            cv2.circle(frame, (220 + int(centro_x), cy), 10, (255,255,255), -1)
+            
+            # Calcule a relação em relação ao centro da imagem
+            relacao = (int(centro_x) - cx_roi ) / cx_roi
+            cv2.putText(frame, f"Distancia x: {int(centro_x)}, Distancia y: {int(centro_y)}", (0, 50), 2, 1, (0, 0, 255), 2)
+            print("Relação em relação ao centro da imagem (-1 a 1):", relacao)
+        else:
+            print("Nenhum pixel branco na região de interesse.")
+            #vou fazer uma rotina para ele ira atras do ponto vemelho aqui
     else:
-        print("Nenhum pixel branco na região de interesse.")
-    
+        print("Nenhum contorno encontrado na região de interesse.")
+        #vou fazer uma rotina para ele ira atras do ponto vemelho aqui
+
     cv2.imshow("Red", red_mask)
     cv2.imshow("parte", frame)
 
