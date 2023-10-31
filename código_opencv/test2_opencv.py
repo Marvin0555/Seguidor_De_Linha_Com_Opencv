48c0a70ea61f5dcb686990351a701447f3903676
Marvin0555 <60906316+Marvin0555@users.noreply.github.com>
2023-10-30 23:45:25 -0400

    criação da função de detecção da trilha

--- código_opencv/test2_opencv.py
+++ código_opencv/test2_opencv.py
@@ -5,18 +5,37 @@ import time
 cap = cv2.VideoCapture(2)
 Starus_esteria = "OFF"
 
-while True:
+def detectar_cor(mascara_hsv,frame_tela):
+    if mascara_hsv:
+        # Se houver contornos encontrados
+        for caminho in mascara_hsv:
+            # Faça algo com os contornos
+            (x, y, w, h) = cv2.boundingRect(caminho)
+            
+            area = cv2.contourArea(caminho)
+            if area > 400:
+                cv2.rectangle(frame_tela, (x, y), (x + w, y + h), (255, 255, 255), 2)
+                moment = cv2.moments(caminho)
+                area = moment['m00']
+                qcentro_x = int(moment['m10']/area)
+                qcentro_y = int(moment['m01']/area)
+                cv2.circle(frame_tela, (qcentro_x , qcentro_y), 10, (0,0,0), -1)
+        
+    else:
+        print("Não tem vermelho")
 
+while True:
     _, frame = cap.read()
+    _, detectar = cap.read()
     height, width, dd = frame.shape
     cx = int(width // 2)
     cy = int(height // 2)
 
     belt = frame[0: height, cx-100: cx+100 ]
+
     mask = cv2.cvtColor(belt, cv2.COLOR_BGR2HSV)
-
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
+    hsv_detectar = cv2.cvtColor(detectar, cv2.COLOR_BGR2HSV)
 
     # Extraia a região de interesse
     cv2.circle(frame, (cx , cy), 10, (0,255,0), -1)
@@ -25,9 +44,10 @@ while True:
     cv2.line(frame, (cx-100, cy), (cx+100, cy), (100, 30, 170), 3)
 
     # Red color
-    low_red = np.array([161, 155, 84])
-    high_red = np.array([179, 255, 255])
+    low_red = np.array([99, 108, 131]) #np.array([161, 155, 84])
+    high_red = np.array([180, 255, 255]) #np.array([179, 255, 255])
     red_mask = cv2.inRange(mask, low_red, high_red)
+    detectar_red = cv2.inRange(hsv_detectar, low_red, high_red)
     
     #erosão e dilatação
     erodeElement = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
@@ -35,11 +55,11 @@ while True:
 
     #erosão de rgb
     red_mask = cv2.erode(red_mask,erodeElement)
+    detectar_red = cv2.erode(detectar_red,erodeElement)
 
     #dilatação de rgb
     red_mask = cv2.erode(red_mask,dilateElement)
-
-    
+    detectar_red = cv2.erode(detectar_red,dilateElement)
 
     # Extraia a região de interesse
     roi = red_mask[cy-1:cy+1, 1:200]
@@ -48,7 +68,8 @@ while True:
     posicao_centro = np.argwhere(roi == 255)
 
     red_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #print(red_contours)
+    detectar_red_contours, _ = cv2.findContours(detectar_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     if red_contours:
         # Se houver contornos encontrados
         for cnt in red_contours:
@@ -65,7 +86,6 @@ while True:
                 qcentro_y = int(moment['m01']/area)
                 #cv2.circle(frame, (220 + qcentro_x , qcentro_y), 10, (0,255,0), -1)
 
-
         # Verifique se há pixels brancos na ROI
         if len(posicao_centro) > 0:
             y,x = roi.shape
@@ -78,20 +98,18 @@ while True:
             # Calcule a relação em relação ao centro da imagem
             relacao = (int(centro_x) - cx_roi ) / cx_roi
             cv2.putText(frame, f"Distancia x: {int(centro_x)}, Distancia y: {int(centro_y)}", (0, 50), 2, 1, (0, 0, 255), 2)
-            print("Relação em relação ao centro da imagem (-1 a 1):", relacao)
+            print("Relação  do centro dos pixles em relação ao centro da imagem (-1 a 1):", relacao)
         else:
             print("Nenhum pixel branco na região de interesse.")
-            #vou fazer uma rotina para ele ira atras do ponto vemelho aqui
+            detectar_cor(detectar_red_contours,detectar)
     else:
         print("Nenhum contorno encontrado na região de interesse.")
-        #vou fazer uma rotina para ele ira atras do ponto vemelho aqui
+        detectar_cor(detectar_red_contours,detectar)
 
     cv2.imshow("Red", red_mask)
     cv2.imshow("parte", frame)
+    cv2.imshow("Detectar", detectar)
 
     key = cv2.waitKey(1)
     if key == 27:
         break
-
-
-
